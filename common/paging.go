6add52d1973124329b5b1d7e52bfd0d1bec59dc5
thientantran <thientantm@gmail.com>
2024-06-20 21:21:54 +0700

    Page with seek method, not using offset clause

--- common/paging.go
+++ common/paging.go
@@ -6,6 +6,8 @@ type Paging struct {
 	Page  int   `json:"page" form:"page"`
 	Limit int   `json:"limit" form:"limit"`
 	Total int64 `json:"total" form:"total"`
+	// tối ưu việc paging, bình thường thì lay page rồi tính toán số rows cần bỏ (Offset clause) => dùng limit, offset
+	// tuy nhiên cách đó hơi lâu, khi số lương rows tăng vì phải scan qua hết số row đó => đổi qua dùng seek method, dùng cursor
 
 	FakeCursor string `json:"cursor" form:"cursor"`
 	NextCursor string `json:"next_cursor"`
--- module/restaurant/storage/list.go
+++ module/restaurant/storage/list.go
@@ -6,6 +6,16 @@ import (
 	"context"
 )
 
+// hệ thống tăng tải
+// step 1: fetch 1000 ids => catching
+// step 2: fetch 50 first ids => data page 1
+// step 3: page2, omit 50 ids and fetch the next 50ids
+// khi cache hết hạn thì làm lại step1
+// điểm yếu: dữ liệu thường xuyên ko thay đổi
+// điểm mạnh: tăng tải, giảm db query
+// Limit OFFSET có 1 vài vấn đề do nó chỉ nhớ số dòng => nên dùng cursor
+// có 1 vấn đề, khi bấm trang 2, đúng lúc db thay đổi, sẽ có dữ liệu thêm vào, gây dữ liệu qua trang, qua dòng, gây ra page có dữ liệu của page 1, load lại page 1 thì ko thấy do để qua số dòng
+// ngược lại khi xoá thì 1 số dữ liệu page 2 nhảy lên, do đó, đang từ page 1 chuyển sang page 2 sẽ có dấu hiệu thiếu data
 func (s *sqlStore) ListDataWithCondition(
 	context context.Context,
 	filter *restaurantmodel.Filter,
@@ -28,9 +38,25 @@ func (s *sqlStore) ListDataWithCondition(
 		return nil, common.ErrDB(err)
 	}
 
-	offset := (paging.Page - 1) * paging.Limit
-	if err := db.Offset(offset).Limit(paging.Limit).Order("id desc").Find(&result).Error; err != nil {
+	if v := paging.FakeCursor; v != "" {
+		uid, err := common.FromBase58(v)
+		if err != nil {
+			return nil, common.ErrDB(err)
+		}
+		db = db.Where("id < ?", uid.GetLocalID())
+	} else {
+		offset := (paging.Page - 1) * paging.Limit
+		db.Offset(offset)
+	}
+	if err := db.Limit(paging.Limit).Order("id desc").Find(&result).Error; err != nil {
 		return nil, common.ErrDB(err)
 	}
+
+	if len(result) > 0 {
+		last := result[len(result)-1]
+		last.Mask(false)
+		paging.NextCursor = last.FakeId.String()
+	}
+
 	return result, nil
 }
