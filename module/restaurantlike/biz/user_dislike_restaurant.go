6929c24c450225a83167347039bd8b84531a7d9c
thientantran <thientantm@gmail.com>
2024-07-12 22:31:57 +0700

    Dislike - API

--- module/restaurantlike/biz/user_dislike_restaurant.go
+++ module/restaurantlike/biz/user_dislike_restaurant.go
@@ -1,22 +1,31 @@
 package rstlikebiz
 
 import (
+	"Food-delivery/common"
 	restaurantlikemodel "Food-delivery/module/restaurantlike/model"
 	"context"
+	"log"
+	"time"
 )
 
 type UserDislikeRestaurantStore interface {
 	Delete(ctx context.Context, userId, restaurantId int) error
 }
 
+type DecreaseLikeCountStore interface {
+	DecreaseLikeCount(ctx context.Context, id int) error
+}
+
 type userDislikeRestaurantBiz struct {
-	store UserDislikeRestaurantStore
+	store    UserDislikeRestaurantStore
+	decStore DecreaseLikeCountStore
 }
 
 func NewUserDisLikeRestaurantBiz(
 	store UserDislikeRestaurantStore,
+	decStore DecreaseLikeCountStore,
 ) *userDislikeRestaurantBiz {
-	return &userDislikeRestaurantBiz{store: store}
+	return &userDislikeRestaurantBiz{store: store, decStore: decStore}
 }
 
 func (biz *userDislikeRestaurantBiz) DislikeRestaurant(
@@ -29,5 +38,14 @@ func (biz *userDislikeRestaurantBiz) DislikeRestaurant(
 		return restaurantlikemodel.ErrCannotDisLikeRestaurant(err)
 	}
 
+	go func() {
+		defer common.AppRecover()
+		// ví dụ cái này tốn time để xử lý
+		time.Sleep(3 * time.Second)
+		if err := biz.decStore.DecreaseLikeCount(ctx, restaurantId); err != nil {
+			log.Println(err)
+			// ko panic gì hết,
+		}
+	}()
 	return nil
 }
--- module/restaurantlike/transport/ginrstlike/user_dislike_restaurant.go
+++ module/restaurantlike/transport/ginrstlike/user_dislike_restaurant.go
@@ -3,6 +3,7 @@ package ginrstlike
 import (
 	"Food-delivery/common"
 	"Food-delivery/component/appctx"
+	restaurantstorage "Food-delivery/module/restaurant/storage"
 	rstlikebiz "Food-delivery/module/restaurantlike/biz"
 	restaurantlikestorage "Food-delivery/module/restaurantlike/storage"
 	"github.com/gin-gonic/gin"
@@ -20,8 +21,8 @@ func UserDislikeRestaurant(appCtx appctx.AppContext) gin.HandlerFunc {
 		requester := c.MustGet(common.CurrentUser).(common.Requester)
 
 		store := restaurantlikestorage.NewSQLStore(appCtx.GetMainDBConnection())
-
-		biz := rstlikebiz.NewUserDisLikeRestaurantBiz(store)
+		decStore := restaurantstorage.NewSQLStore(appCtx.GetMainDBConnection())
+		biz := rstlikebiz.NewUserDisLikeRestaurantBiz(store, decStore)
 
 		if err := biz.DislikeRestaurant(c.Request.Context(), requester.GetUserId(), int(uid.GetLocalID())); err != nil {
 			panic(err)
