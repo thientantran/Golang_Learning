e77de3a99fcf3675e9e70baa575c9ea140c658d7
thientantran <thientantm@gmail.com>
2024-07-17 23:24:17 +0700

    Custom trace to get more information

--- module/restaurant/biz/list_restaurant.go
+++ module/restaurant/biz/list_restaurant.go
@@ -29,6 +29,11 @@ func (biz *listRestaurantBiz) ListRestaurant(
 	paging *common.Paging,
 ) ([]restaurantmodel.Restaurant, error) {
 	ctx1, span := trace.StartSpan(ctx, "biz.list_restaurant")
+	span.AddAttributes(
+		trace.Int64Attribute("page", int64(paging.Page)),
+		trace.Int64Attribute("limit", int64(paging.Limit)),
+		trace.StringAttribute("cursor", paging.FakeCursor),
+	)
 	// phải truyền lại ctx1 thay vì ctx, để jaeder phân cấp được cái span nào trong span nào, nghiệp vụ nào trong nghiệp vụ nào
 	// slice đã là reference type (con trỏ rồi) nên không cần dùng pointer
 	result, err := biz.repo.ListRestaurant(ctx1, filter, paging)
